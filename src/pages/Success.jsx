86ee60a3af9dd694cc6a16de9c2a34e848baf314
Endks <mientrasde78@gmail.com>
2026-02-25 16:08:16 -0500

    corrigiendo error de redireccion a success

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,8 +1,13 @@
+import { useSearchParams } from "react-router-dom";
+
 export default function Success() {
+  const [params] = useSearchParams();
+
   return (
     <div>
-      <h1>Pago exitoso 🎉</h1>
-      <p>Gracias por tu compra</p>
+      <h1>✅ Pago exitoso</h1>
+      <p>Estado: {params.get("redirect_status")}</p>
+      <p>PaymentIntent: {params.get("payment_intent")}</p>
     </div>
   );
 }
